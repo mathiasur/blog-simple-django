adf66990a609e8c8a20cf31f0fb59424ffa741fa
Mathias Urtheil <mathias.urtheil@gmail.com>
2023-08-02 22:06:23 -0300

    create post form

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BlogListView
+from .views import BlogListView, PostCreateView
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='home')
+    path('', BlogListView.as_view(), name='home'),
+    path('create/', PostCreateView.as_view(), name='create')
 ]
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
+from .forms import PostCreateForm
 
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
@@ -8,3 +9,15 @@ class BlogListView(View):
         }
         return render(request, 'blog_list.html', context)
 
+class PostCreateView(View):
+    def get(self, request, *args, **kwargs):
+        context = {
+
+        }
+        return render(request, 'post_create.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        context = {
+
+        }
+        return render(request, 'post_create.html', context)
